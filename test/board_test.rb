49ed6ad986ca1db07d08179cbff3a4ebbb260505
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-02-05 16:27:41 -0700

    Resolve conflicts

--- test/board_test.rb
+++ test/board_test.rb
@@ -14,6 +14,14 @@ class BoardTest < Minitest::Test
   end
 
 
+  def test_it_has_cells
+
+    assert_instance_of Hash, @board.cells
+    assert_instance_of Cell, @board.cells["A1"]
+    assert_instance_of Cell, @board.cells["D4"]
+    assert_nil @board.cells["D5"]
+  end
+
 
 
 
