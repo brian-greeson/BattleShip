8395a975897e6a80f5e5df495555eff3beaa03af
Josh Tukman <josh.tukman@gmail.com>
2020-02-05 15:51:43 -0700

    Add exist test for Board class

--- test/board_test.rb
+++ test/board_test.rb
@@ -8,10 +8,14 @@ require './lib/board'
 class BoardTest < Minitest::Test
 
   def setup
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)
-  end 
+    @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
+  end
+
+  def test_board_has_cells
+    assert_instance_of Board, @board 
+  end
 
 
 
