6f8f29d456464f042857aaac0fd8ff1b0a05ee46
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-02-05 18:48:53 -0700

    Update test_valid_coordinate with proper instance variable @

--- test/board_test.rb
+++ test/board_test.rb
@@ -25,13 +25,13 @@ class BoardTest < Minitest::Test
     assert_nil @board.cells["D5"]
   end
 
-  def test_valid_coordinate?_knows_if_a_coordinate_is_on_the_board
+  def test_valid_coordinate_knows_if_a_coordinate_is_on_the_board
 
-    assert_equal true, board.valid_coordinate?("A1")
-    assert_equal true, board.valid_coordinate?("D4")
-    assert_equal false, board.valid_coordinate?("A5")
-    assert_equal false, board.valid_coordinate?("E1")
-    assert_equal false, board.valid_coordinate?("A22")
+    assert_equal true, @board.valid_coordinate?("A1")
+    assert_equal true, @board.valid_coordinate?("D4")
+    assert_equal false, @board.valid_coordinate?("A5")
+    assert_equal false, @board.valid_coordinate?("E1")
+    assert_equal false, @board.valid_coordinate?("A22")
   end
 
 
